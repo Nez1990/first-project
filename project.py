a25a2550af1094277fcca071cf4711a6f9e16ee9
Nezih <nezih-senal.guel@dci-student.org>
2025-01-08 15:33:36 +0100

    changes

--- project.py
+++ project.py
@@ -10,8 +10,9 @@ height= float(input("Input your current height in cm:",))
 print(f'You are {gender} who is {height} cm tall and weighing {weight} kg')
 
 #3. I calculate his BMI with his height and weight and print the message according to his BMI taking th gender into account for different tips
-def calculate_my_BMI(height, weight,gender):
+def calculate_my_BMI(height, weight, gender):
     BMI = weight/height**2
+    
     if BMI<18.5 and gender=="Male":
         print(f'You have a BMI of {round(BMI,2)} which is Underweight\nYou need to eat more Buddy!')
         print(calorie_tips["Men"])
@@ -21,6 +22,8 @@ def calculate_my_BMI(height, weight,gender):
     elif BMI >25 and gender == "Male":
         print(f'You have a BMI of {round(BMI,2)} which is Overweight\nStop eating this much Buddy!')
         print(calorie_tips["Men"])
+
+
     elif BMI<18.5 and gender=="Female":
         print(f'You have a BMI of {round(BMI,2)} which is Underweight\nYou need to eat more Lady!')
         print(calorie_tips["Women"])
@@ -30,6 +33,12 @@ def calculate_my_BMI(height, weight,gender):
     elif BMI >25 and gender == "Female":
         print(f'You have a BMI of {round(BMI,2)} which is Overweight\nStop eating this much, Lady')
         print(calorie_tips["Women"])
+    
+    return BMI
+
+# def optimal_BMI(BMI, height, weight):
+#     weight=BMI*height**2
+    
 
 
 
@@ -37,7 +46,6 @@ def calculate_my_BMI(height, weight,gender):
 calorie_tips = {
     "Men":"As a Man you should focus on strength training to build muscles",
     "Women":"As a woman, you may have more body fat compared to men at the same BMI, so be sure to check other health markers.",
-    
 }
 
 #5. I call the function
